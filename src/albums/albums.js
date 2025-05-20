7ed573f1aefe196e66df87bb8fb2b2db0aec62e3
Zbigniew Jan Stepniewski <zbigi@zbigiman.com>
2025-05-20 11:46:48 +0200

    move audio to public

--- src/albums/albums.js
+++ src/albums/albums.js
@@ -5,7 +5,7 @@ export const ALBUMS = [
         cover: 'nas-life-is-a-bitch-remix-cover.jpg',
         tracks: [
             {
-                audio: '/audio/nas-life-a-bitch-remix/nas-life-s-a-bitch-remix.flac',
+                audio: 'audio/nas-life-a-bitch-remix/nas-life-s-a-bitch-remix.flac',
                 artist: 'Zibi Funkowicz',
                 title: 'NAS Life is a Bitch Z-CLASSICS remix',
 
@@ -18,7 +18,7 @@ export const ALBUMS = [
         cover: 'method-man-z-funk-remix.jpg',
         tracks: [
             {
-                audio: '/audio/methnod-man-bring-the-pain-z-funk-remix/bring-da-pain-remix.flac',
+                audio: 'audio/methnod-man-bring-the-pain-z-funk-remix/bring-da-pain-remix.flac',
                 artist: 'Zibi Funkowicz',
                 title: 'Method Man Bring the Pain Z-FUNK remix',
 
@@ -31,54 +31,54 @@ export const ALBUMS = [
         cover: 'the-importance-of-the-music.jpg',
         tracks: [
             {
-                audio: '/audio/the-importance-of-music/00-the-importance-of-music.flac',
+                audio: 'audio/the-importance-of-music/00-the-importance-of-music.flac',
                 artist: 'Zibi Funkowicz',
                 title: 'The Importance of Music',
 
             },
             {
-                audio: '/audio/the-importance-of-music/01-Zibi-Funkowicz-Z-funk-No-Shit-Worldwide-Hardcore-Forever.flac',
+                audio: 'audio/the-importance-of-music/01-Zibi-Funkowicz-Z-funk-No-Shit-Worldwide-Hardcore-Forever.flac',
                 artist: 'Zibi Funkowicz',
                 title: 'Z-FUNK NO SHIT WORLDWIDE FORVER',
 
             },
             {
-                audio: '/audio/the-importance-of-music/02-Zibi-Funkowicz-2003-Guitars-One.flac',
+                audio: 'audio/the-importance-of-music/02-Zibi-Funkowicz-2003-Guitars-One.flac',
                 artist: 'Zibi Funkowicz',
                 title: '2003 Guitars One',
             },
             {
-                audio: '/audio/the-importance-of-music/03-Zibi-Funkowicz-2003-Guitars-Two.flac',
+                audio: 'audio/the-importance-of-music/03-Zibi-Funkowicz-2003-Guitars-Two.flac',
                 artist: 'Zibi Funkowicz',
                 title: '2003 Guitars Two',
             },
             {
-                audio: '/audio/the-importance-of-music/04-Zibi-Funkowicz-2003-Guitars-Three.flac',
+                audio: 'audio/the-importance-of-music/04-Zibi-Funkowicz-2003-Guitars-Three.flac',
                 artist: 'Zibi Funkowicz',
                 title: '2003 Guitars Three',
             },
             {
-                audio: '/audio/the-importance-of-music/05-Zibi-Funkowicz-2003-Guitars-Four.flac',
+                audio: 'audio/the-importance-of-music/05-Zibi-Funkowicz-2003-Guitars-Four.flac',
                 artist: 'Zibi Funkowicz',
                 title: '2003 Guitars Four',
             },
             {
-                audio: '/audio/the-importance-of-music/06-Zibi-Funkowicz-World_is_a_Ghetto.flac',
+                audio: 'audio/the-importance-of-music/06-Zibi-Funkowicz-World_is_a_Ghetto.flac',
                 artist: 'Zibi Funkowicz',
                 title: 'World is a Ghetto',
             },
             {
-                audio: '/audio/the-importance-of-music/07-Zibi-Funkowicz-Going-Gome-Instrumental.flac',
+                audio: 'audio/the-importance-of-music/07-Zibi-Funkowicz-Going-Gome-Instrumental.flac',
                 artist: 'Zibi Funkowicz',
                 title: 'Going Home Instrumental',
             },
             {
-                audio: '/audio/the-importance-of-music/08-Zibi-Funkowicz-All-Funk.flac',
+                audio: 'audio/the-importance-of-music/08-Zibi-Funkowicz-All-Funk.flac',
                 artist: 'Zibi Funkowicz',
                 title: 'All Funk',
             },
             {
-                audio: '/audio/the-importance-of-music/09-Z-Funk-No-Shit-Worldwide-Hardcore-Forever-Flashback.flac',
+                audio: 'audio/the-importance-of-music/09-Z-Funk-No-Shit-Worldwide-Hardcore-Forever-Flashback.flac',
                 artist: 'Zibi Funkowicz',
                 title: 'Z Funk No Shit Worldwide Hardcore Forever Flashback',
             }
@@ -90,32 +90,32 @@ export const ALBUMS = [
         cover: 'caminos-tortuosos-album-cover.jpg',
         tracks: [
             {
-                audio: '/audio/caminos-tortuosos/01_caminos_tortuosos.flac',
+                audio: 'audio/caminos-tortuosos/01_caminos_tortuosos.flac',
                 artist: 'Zbigniew Jan Stępniewski & Elena Mota García',
                 title: 'Caminos Tortuosos',
             },
             {
-                audio: '/audio/caminos-tortuosos/02_fire_in_the_soul.flac',
+                audio: 'audio/caminos-tortuosos/02_fire_in_the_soul.flac',
                 artist: 'Zbigniew Jan Stępniewski & Elena Mota García',
                 title: 'Fire in the Soul',
             },
             {
-                audio: '/audio/caminos-tortuosos/03_la_mar_de_sangre.flac',
+                audio: 'audio/caminos-tortuosos/03_la_mar_de_sangre.flac',
                 artist: 'Zbigniew Jan Stępniewski & Elena Mota García',
                 title: 'La Mar de Sangre',
             },
             {
-                audio: '/audio/caminos-tortuosos/04_la_mar_de_lagrimas.flac',
+                audio: 'audio/caminos-tortuosos/04_la_mar_de_lagrimas.flac',
                 artist: 'Zbigniew Jan Stępniewski & Elena Mota García',
                 title: 'La Mar de Lagrimas',
             },
             {
-                audio: '/audio/caminos-tortuosos/05_little_hope.flac',
+                audio: 'audio/caminos-tortuosos/05_little_hope.flac',
                 artist: 'Zbigniew Jan Stępniewski & Elena Mota García',
                 title: 'Little Hope',
             },
             {
-                audio: '/audio/caminos-tortuosos/06_do_what_you_love.flac',
+                audio: 'audio/caminos-tortuosos/06_do_what_you_love.flac',
                 artist: 'Zbigniew Jan Stępniewski & Elena Mota García',
                 title: 'Do What You Love',
             }
